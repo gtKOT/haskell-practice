3b13a6084fa7f8c9d9bcb1a9eb9c78195a6589d3
KOT <askastr@gmail.com>
2014-09-28 15:21:42 +0900

    module

--- src/chapter7.hs
+++ src/chapter7.hs
@@ -1,3 +1,12 @@
+module Shapes (
+  Point(..),
+  Shape(..),
+  area,
+  nudge,
+  baseCircle,
+  baseRect
+) where
+
 data Shape' = Circle' Float Float Float |
               Rectangle' Float Float Float Float
   deriving (Show)
@@ -13,7 +22,7 @@ data Point = Point Float Float deriving (Show)
 data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
 
 area :: Shape -> Float
-area (Circle _ r) = pi * r ^2
+area (Circle _ r) = pi * r ^ 2
 area (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
 
 nudge :: Shape -> Float -> Float -> Shape
