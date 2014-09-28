cfede11dc4d6179a267f35c30b3b05de16ac94c8
KOT <askastr@gmail.com>
2014-09-28 14:14:36 +0900

    area関数

--- src/chapter7.hs
+++ src/chapter7.hs
@@ -2,5 +2,7 @@ data Shape = Circle Float Float Float |
              Rectangle Float Float Float Float
 -- これだけで、CircleとRectangleは、値コンストラクタとして、他から認識される。
 
-
+area :: Shape -> Float
+area (Circle _ _ r) = pi * r ^ 2
+area (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
 
